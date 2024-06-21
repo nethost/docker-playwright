6f1c7e1b0df3f673e0e171121494e06245e1d2ec
billgo <cocobill@vip.qq.com>
2024-06-21 15:06:51 +0800

    update

--- 1.40/build.sh
+++ 1.40/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64" latest chromium firefox webkit chrome msedge browsers
+docker buildx bake --push --set "*.platform=linux/arm64/v8,linux/amd64" latest
